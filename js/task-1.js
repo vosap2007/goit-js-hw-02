d69a130d629e02dc1ed0d66b6bc559e59fe107a5
Volodymyr Udovychenko <vosap2007@i.ua>
2020-08-25 00:08:57 +0300

    завершает все дз

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+const getItemsString = function (array) {
+  let result = "";
+
+  for (let i = 0; i < array.length; i += 1) {
+    result += `${i + 1} - ${array[i]}\n`;
+  }
+  return result;
+};
+
+console.log(getItemsString(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]));
---
+++ js/task-3.js
@@ -0,0 +1,20 @@
+function findLongestWord(string = "") {
+  let words = string.split(" ");
+  let LongestWord = words[0];
+
+  for (const word of words) {
+    if (word.length > LongestWord.length) {
+      LongestWord = word;
+    }
+  }
+  return LongestWord;
+}
+
+console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
+// 'jumped'
+
+console.log(findLongestWord("Google do a roll"));
+// 'Google'
+
+console.log(findLongestWord("May the force be with you"));
+// 'force'
